94dd17db720f37e83d173b173654a6118ed179e7
Ekaterina Zinchenko <ekaterinazinchenko0706@gmail.com>
2021-07-23 23:23:19 +0300

    Закончила работу над js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,25 +5,52 @@ let cartCounter = 0;
 let cartPrice = 0;
 
 const incrementCounter = () => {
-  cartCounterLabel.innerHTML = '${++cartCounter}';
+  cartCounterLabel.innerHTML = `${++cartCounter}`;
   if (cartCounter === 1) cartCounterLabel.style.display = 'block';
 };
 
+const getMockData = (t) => +t.parentElement
+  .previousElementSibling
+  .innerHTML
+  .replace(/^\$(\d+)\s\D+(\d+).*$/, '$1.$2');
+
+const getPrice = (t, price) => Math.round((cartPrice + getMockData(t)) * 100) / 100;
+
+const disableControls = (t, fn) => {
+  contentContainer.removeEventListener('click', fn);
+    t.disabled = true;
+}
+const enableControls = (t, fn) => {
+    t.disabled = false;
+    contentContainer.addEventListener('click', fn);
+}
+
+
 const btnClickHandler = (e) => {
   const target = e.target;
+  const interval = 2000;
+
+  let restoreHtml = null;
 
   if (target && target.matches('.item-actions__cart')) {
     incrementCounter();
 
-    const mockData = +target.parentElement.previousElementSibling.innerHTML.replace(/^\$(\d+)\s\D+(\d+).*$/, '$1.$2');
+    cartPrice = getPrice(target, cartPrice);
 
-    cartPrice = Math.round((cartPrice + mockData) * 100) / 100;
+    restoreHtml = target.innerHTML;
 
     target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
 
     //ниже получаем число, без лишних символов, при нажатии на кнопку
     // console.log(+target.parentElement.previousElementSibling.innerHTML.replace('$', '').replace(' <sup>', '.').replace('</sup>', ''));
+    
+    disableControls(target, btnClickHandler);
+
+    setTimeout(() => {
+      target.innerHTML = restoreHtml;
+      enableControls(target, btnClickHandler);
+    }, interval);
   }
 };
 
-contentContainer.addEventListener('click', btnClickHandler)
+contentContainer.addEventListener('click', btnClickHandler);
